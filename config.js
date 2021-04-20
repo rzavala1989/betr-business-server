6ea8e40862493a85149c62cbdc31570c7fdd81ce
Ricardo Zavala <zavala.ricardo.m@gmail.com>
2021-04-19 21:07:46 -0700

    cors

--- config.js
+++ config.js
@@ -9,4 +9,4 @@ exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || "7d";
 exports.CLIENT_ORIGIN =
-  /*'https://desolate-taiga-15973.herokuapp.com' ||*/ "http://localhost:3000";
+  "https://desolate-taiga-15973.herokuapp.com" || "http://localhost:3000";
